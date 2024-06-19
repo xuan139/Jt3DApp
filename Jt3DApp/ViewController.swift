f6602ec8635ab89413ff5c2354da700cc1e563db
xuan <13901996631@139.com>
2024-06-18 23:08:06 -0500

    Initial Commit

---
+++ Jt3DApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Jt3DApp
+//
+//  Created by XUAN LI on 6/18/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
